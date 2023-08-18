f2d48dc371106a20335e7655a34d7665dc061f0e
miloszcichy <milosz.cichy@outlook.com>
2023-08-18 12:25:33 +0200

    interview v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,6 @@
 
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import {
-    combineLatest,
-    firstValueFrom,
-    Observable,
-    Subject,
-    takeUntil,
-} from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, Subject} from 'rxjs';
 import { PrioritiesApi } from '../service/description/priorities.api';
 import { Content } from '../model/content.model';
 import { Permission } from '../model/permission.enum';
@@ -17,11 +10,7 @@ import { ContentApi } from '../service/content/content.api';
 import { DecodeService } from '../service/decode/decode.service';
 import { PermissionsApi } from '../service/permissions/permissions.api';
 import { UserApi } from '../service/user/user.api';
-import {
-    data,
-    Request,
-    RequestState,
-} from '../model/typescript-assignemt-data';
+import { data, Request, RequestState} from '../model/typescript-assignemt-data';
 
 @Component({
     selector: 'my-app',
@@ -66,10 +55,6 @@ export class AppComponent implements OnInit, OnDestroy {
 
     /*1.Using UserApi#getCurrentUser() fetch user and assign it to the field user.*/
     private getUser() {
-        this.userApi
-            .getCurrentUser()
-            .pipe(takeUntil(this.onDestroy$))
-            .subscribe((value) => (this.user = value));
     }
 
     /*2.Using UserApi#getCurrentUser() and PermissionsApi#getPermissions() create new observable "hasPermission".
@@ -77,14 +62,6 @@ export class AppComponent implements OnInit, OnDestroy {
         Next you need to call UserPermissions#hasPermission(userId, Permission.DISPLAY_CONTENT): boolean.
     */
     private getUserPermission() {
-        this.hasPermission$ = combineLatest([
-            this.userApi.getCurrentUser(),
-            this.permissionsApi.getPermissions(),
-        ]).pipe(
-            map(([user, permissions]) =>
-                permissions.hasPermission(user.id, Permission.DISPLAY_CONTENT)
-            )
-        );
     }
 
     /*3.
@@ -95,18 +72,6 @@ export class AppComponent implements OnInit, OnDestroy {
         To create TableContentItem#description use pattern "Entry: Content#description, Department: decoded_Department, Location: decoded_Location"
       -Assign the result to field tableContentItems */
     private getTableContent() {
-        this.contentApi
-            .getContent()
-            .pipe(takeUntil(this.onDestroy$))
-            .subscribe((contentList: Content[]) => {
-                Promise.all(
-                    contentList.map(async (content) => {
-                        const department = await this.getDecodedValue(content.department);
-                        const location = await this.getDecodedValue(content.location);
-                        return this.buildContentTableItem(content, department, location);
-                    })
-                ).then((result) => (this.contentTableItems = result));
-            });
     }
 
     /**4. Use PrioritiesApi#getPriorities and map incoming data to given format:
@@ -116,22 +81,6 @@ export class AppComponent implements OnInit, OnDestroy {
      * Priorities: Very High, Urgent, Do not ignore
      */
     private getPriorities() {
-        this.priorities$ = this.prioritiesApi
-            .getPriorities()
-            .pipe(
-                map((priorities) => priorities.reduce((acc, val) => `${acc}, ${val}`))
-            );
-    }
-
-    private buildContentTableItem({ id, description }: Content, department, location) {
-        return {
-            id: id,
-            description: `Entry: ${description}, Department: ${department}, Location: ${location}`,
-        };
-    }
-
-    private async getDecodedValue(code: number): Promise<string> {
-        return await firstValueFrom(this.decodeService.instant(code));
     }
 
     /**
@@ -143,21 +92,8 @@ export class AppComponent implements OnInit, OnDestroy {
      * If yes log to console "success", otherwise log to console a list of all invalid invoices in fromat of {id: n, userId: m}.
      */
     private assignment1() {
-        let result = null;
-        const condition = this.filterStatus().every((object) => object.invoice.isValid);
-        result = condition ? 'success' : this.extractInvalidInvoices();
-        console.log(result);
-
     }
 
-    private extractInvalidInvoices(){
-      return this.filterStatus()
-        .map((object) => object.invoice)
-        .filter((object) => !object.isValid)
-        .map(({id, userId}) => {
-            return { id, userId };
-        });
-    }
     /**
      * 2.
      * -Add new Request to existing list (variable data) at position 0.
@@ -168,16 +104,5 @@ export class AppComponent implements OnInit, OnDestroy {
      *
      */
     private assignemnt2() {
-        const newRequest = { id: 10, description: 'new description', title: 'new title' } as Request;
-        const modifiedData = [newRequest, ...data];
-        const foundRequest = modifiedData.find(request => request.id === 10) || newRequest;
-        const { id, title } = foundRequest;
-        console.log(id, title);
-    }
-
-    private filterStatus(): Request[] {
-        return data.filter((object: Request) =>
-            [RequestState.PLANNED, RequestState.COMPLETED].includes(object.state)
-        );
     }
 }
